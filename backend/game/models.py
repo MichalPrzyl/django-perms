231f767cdf8b01c58d54dea17c19ba8720b1b8bc
Michał Przyłucki <michal.przyl@gmail.com>
2024-09-04 20:00:36 +0200

    Apps with initial access file

--- backend/game/models.py
+++ backend/game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField()
+
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Permission(models.Model):
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+
